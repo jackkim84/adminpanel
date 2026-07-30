374682c4bbdac2272fd59256b14308e598c06410
jacks <nana728202@gmail.com>
2026-07-30 19:08:42 +0900

    12 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  // 리액트 플러그인 장착
+  plugins: [react()],
+  
+  build: {
+    // 1. 내장 초고속 압축 엔진(esbuild) 활성화
+    minify: 'esbuild', 
+    
+    rollupOptions: {
+      output: {
+        // 2. 무거운 외부 라이브러리들을 잘게 쪼개어 번들 용량을 경량화하는 표준 로직입니다.
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
